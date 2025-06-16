31ed0cb1894ba7bccac121217445f43f07bf163c
cherish0-0 <jiyu01160@naver.com>
2025-06-16 13:38:26 +0900

    feat(view): 로그인된 회원 전용 홈화면 뷰 템플릿 작성 feat(controller): 로그인 확인 기능 및 엔드포인트 구현

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -1,15 +1,41 @@
 package hello.login.web;
 
+import hello.login.web.member.Member;
+import hello.login.web.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping("/")
+    private final MemberRepository memberRepository;
+
+    // @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/")
+    public String homeLogin(@CookieValue(name="memberId", required = false) Long memberId, Model model) {
+
+        if (memberId == null) {
+            return "home"; // 쿠키가 없으면 홈 페이지로 이동
+        }
+
+        // 로그인
+        Member loginMember = memberRepository.findById(memberId);
+        if (loginMember == null) {
+            return "home"; // 로그인 정보가 없으면 홈 페이지로 이동
+        }
+
+        // 로그인 정보가 있으면 모델에 추가
+        model.addAttribute("member", loginMember);
+        return "loginHome"; // 로그인된 홈 페이지로 이동
     }
 }
