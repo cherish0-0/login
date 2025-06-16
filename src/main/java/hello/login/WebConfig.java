034ea68c3397787135d81523f1ac40c28cec8982
cherish0-0 <jiyu01160@naver.com>
2025-06-16 22:17:10 +0900

    feat(filter): 로그인 인증 체크 필터 구현 feat(Config): WebConfig에서 구현된 필터 설정 (Bean 추가 등록)

--- src/main/java/hello/login/WebConfig.java
+++ src/main/java/hello/login/WebConfig.java
@@ -1,6 +1,7 @@
 package hello.login;
 
 import hello.login.web.filter.LogFilter;
+import hello.login.web.filter.LoginCheckFilter;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +15,16 @@ public class WebConfig {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter()); // LogFilter 인스턴스 생성
         filterRegistrationBean.setOrder(1); // 필터 순서 지정
+        filterRegistrationBean.addUrlPatterns("/*"); // 모든 URL에 필터 적용
+
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean loginCheckFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LoginCheckFilter()); // LoginCheckFilter 인스턴스 생성
+        filterRegistrationBean.setOrder(2); // 필터 순서 지정
         filterRegistrationBean.addUrlPatterns("/*"); // 모든 URL에 필터 적용
 
         return filterRegistrationBean;
---
+++ src/main/java/hello/login/web/filter/LoginCheckFilter.java
@@ -0,0 +1,56 @@
+package hello.login.web.filter;
+
+import hello.login.web.SessionConst;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.PatternMatchUtils;
+
+import java.io.IOException;
+
+@Slf4j
+public class LoginCheckFilter implements Filter {
+
+    private static final String[] whitelist = {"/", "/members/add", "/login", "/logout", "/css/*"};
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String requestURI = httpRequest.getRequestURI();
+
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        try {
+            log.info("로그인 체크 필터 시작 {}", requestURI);
+
+            if (isLoginCheckPath(requestURI)) {
+                log.info("인증 체크 로직 실행 {}", requestURI);
+                HttpSession session = httpRequest.getSession(false);
+                if(session == null || session.getAttribute(SessionConst.LOGIN_MEMBER) == null) {
+                    log.info("미인증 사용자 요청 {}", requestURI);
+                    // 로그인으로 redirect
+                    httpResponse.sendRedirect("/login?redirectURL=" + requestURI);
+                    return; // 필터 체인 중단
+                }
+            }
+
+            // 인증이 완료되었거나, 인증 체크가 필요 없는 요청인 경우 필터 체인을 계속 진행
+            chain.doFilter(request, response);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            log.info("인증 체크 필터 종료 {}", requestURI);
+        }
+    }
+
+    /**
+     * whitelist에 있는 URL은 로그인 체크를 하지 않음
+     */
+    private boolean isLoginCheckPath(String requestURI) {
+        // PatternMatchUtils.simpleMatch() 메서드를 사용하여 요청 URI가 whitelist에 있는지 확인
+        // 요청 URI가 whitelist에 포함되어 있으면 false를 반환하여 로그인 체크를 하지 않음
+        return !PatternMatchUtils.simpleMatch(whitelist, requestURI);
+    }
+}
