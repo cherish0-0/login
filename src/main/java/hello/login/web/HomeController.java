cc331ec49da879cac838c66477351c3f33f8b323
cherish0-0 <jiyu01160@naver.com>
2025-06-16 17:48:08 +0900

    feat(controller): 세션 기능 사용하도록 controller 코드 수정
    - 서블릿이 공식 지원하는 세션으로 변경 필요

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,8 @@ package hello.login.web;
 
 import hello.login.web.member.Member;
 import hello.login.web.member.MemberRepository;
+import hello.login.web.session.SessionManager;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -15,13 +17,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     private final MemberRepository memberRepository;
+    private final SessionManager sessionManager;
 
     // @GetMapping("/")
     public String home() {
         return "home";
     }
 
-    @GetMapping("/")
+    // @GetMapping("/")
     public String homeLogin(@CookieValue(name="memberId", required = false) Long memberId, Model model) {
 
         if (memberId == null) {
@@ -36,6 +39,22 @@ public class HomeController {
 
         // 로그인 정보가 있으면 모델에 추가
         model.addAttribute("member", loginMember);
+        return "loginHome"; // 로그인된 홈 페이지로 이동
+    }
+
+    @GetMapping("/")
+    public String homeLoginV2(HttpServletRequest request, Model model) {
+
+        // 세션 관리자에 저장된 회원 정보 조회
+        Member member = (Member)sessionManager.getSession(request);
+
+
+        if (member == null) {
+            return "home"; // 쿠키가 없으면 홈 페이지로 이동
+        }
+
+        // 로그인 정보가 있으면 모델에 추가
+        model.addAttribute("member", member);
         return "loginHome"; // 로그인된 홈 페이지로 이동
     }
 }
--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -2,7 +2,9 @@ package hello.login.web.login;
 
 import hello.login.domain.login.LoginService;
 import hello.login.web.member.Member;
+import hello.login.web.session.SessionManager;
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.Data;
@@ -19,8 +21,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class LoginController {
 
     private final LoginService loginService;
+    private final SessionManager sessionManager;
 
-    @GetMapping("/login")
+    // @GetMapping("/login")
     public String loginForm(@ModelAttribute("loginForm") LoginForm form) {
         return "login/loginForm"; // 로그인 폼을 보여주는 뷰 이름
     }
@@ -28,7 +31,7 @@ public class LoginController {
     /**
      * HttpServletResponse를 사용해 HTTP 응답을 제어
      */
-    @PostMapping("/login")
+    // @PostMapping("/login")
     public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
         if (bindingResult.hasErrors()) {
             return "login/loginForm"; // 유효성 검사 실패 시 폼으로 돌아감
@@ -51,11 +54,51 @@ public class LoginController {
         return "redirect:/"; // 로그인 성공 후 루트 페이지로 리다이렉트
     }
 
-    @PostMapping("/logout")
+    // @PostMapping("/logout")
     public String logout(HttpServletResponse response) {
         expireCookie(response, "memberId");
         return "redirect:/"; // 로그아웃 후 루트 페이지로 리다이렉트
     }
+
+
+
+    @GetMapping("/login")
+    public String loginFormV2(@ModelAttribute("loginForm") LoginForm form) {
+        return "login/loginForm"; // 로그인 폼을 보여주는 뷰 이름
+    }
+
+    /**
+     * HttpServletResponse를 사용해 HTTP 응답을 제어
+     */
+    @PostMapping("/login")
+    public String loginV2(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
+        if (bindingResult.hasErrors()) {
+            return "login/loginForm"; // 유효성 검사 실패 시 폼으로 돌아감
+        }
+
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+
+        // 로그인 실패 처리 (글로벌 오류)
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+
+        // 로그인 성공 처리
+
+        // 세션 관리자를 통해 세션 생성, 회원 데이터 보관
+        sessionManager.createSession(loginMember, response);
+        return "redirect:/"; // 로그인 성공 후 루트 페이지로 리다이렉트
+    }
+
+    @PostMapping("/logout")
+    public String logoutV2(HttpServletRequest request) {
+        sessionManager.expire(request);
+        return "redirect:/"; // 로그아웃 후 루트 페이지로 리다이렉트
+    }
+
+
+
 
     /**
      * 쿠키를 만료시키는 메소드
