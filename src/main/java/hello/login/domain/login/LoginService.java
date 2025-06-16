5005b3168e45d82729a75fc89a0e3f25158a5e65
cherish0-0 <jiyu01160@naver.com>
2025-06-16 12:08:46 +0900

    feat(service): 회원 로그인 처리 기능 로직 구현

---
+++ src/main/java/hello/login/domain/login/LoginService.java
@@ -0,0 +1,24 @@
+package hello.login.domain.login;
+
+import hello.login.web.member.Member;
+import hello.login.web.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+/**
+ * @Service 어노테이션은 Spring에서 이 클래스를 서비스 컴포넌트로 인식하게 해준다.
+ */
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final MemberRepository memberRepository;
+
+    public Member login(String loginId, String password) {
+        return memberRepository.findByLoginId(loginId)
+                .filter(member -> member.getPassword().equals(password))
+                .orElse(null);
+    }
+}
